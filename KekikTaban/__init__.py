a7a835b6f8898b49e5842f46507355de2003cf14
keyiflerolsun <keyiflerolsun@gmail.com>
2021-03-30 05:22:27 +0300

    🕊

--- KekikTaban/__init__.py
+++ KekikTaban/__init__.py
@@ -4,7 +4,7 @@ YAZAR       = 'keyiflerolsun'
 YAZAR_POSTA = 'keyiflerolsun@gmail.com'
 
 PAKET       = 'KekikTaban'
-VERSIYON    = '0.1.0'
+VERSIYON    = '0.1.1'
 
 REPO        = 'https://github.com/keyiflerolsun/KekikTaban'
 ACIKLAMA    = '@KekikAkademi adına yazılmış Projelerin Terminal Tabanı ve Biraz Dahası'
